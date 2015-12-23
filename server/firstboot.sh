0bea195a96bc9e6a01d3c3f99cfe5a6d3259496a
lordkator <lordkator@users.noreply.github.com>
2015-12-22 19:27:22 -0500

    Log output to syslog for debugging later

--- server/firstboot.sh
+++ server/firstboot.sh
@@ -1,6 +1,8 @@
-
-    apt-get -y install virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
-    export DEBIAN_FRONTEND=noninteractive
-    apt-get update
-    apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
-    apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+#!/bin/bash
+(set -x
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+apt-get -y install virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+) | logger -t firstboot
