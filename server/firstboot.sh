0c9de9bebce0a50467423a3835181f135d12fa21
lordkator <lordkator@users.noreply.github.com>
2015-12-23 08:15:17 -0500

    Add running of patches/* files

--- server/firstboot.sh
+++ server/firstboot.sh
@@ -11,6 +11,12 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get -y -o Dpkg::Options::="--force-confnew" dist-upgrade
 apt-get -y install xfce4 xfce4-goodies lightdm eclipse
+
+for i in $(dirname ${BASH_SOURCE[0]})'/patches/'*
+do
+    echo "** Apply patch $i"
+    (cd /;patch -p0 -Nft) < "$i"
+done
 ) 2>&1 | logger -i -t firstboot -s 2>&1
 
 logger -i -t firstboot "** $0 COMPLETE AFTER $SECOND SECOND(S)"
