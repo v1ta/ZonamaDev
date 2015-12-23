17d1b9e039beab2db768e0ff28d69702a0a95a7a
lordkator <lordkator@users.noreply.github.com>
2015-12-23 06:46:26 -0500

    Add start/complete messages

--- server/firstboot.sh
+++ server/firstboot.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+logger -i -t firstboot "** %0 START"
+
 (set -x
 unset UCF_FORCE_CONFFOLD
 export UCF_FORCE_CONFFNEW=YES
@@ -8,4 +11,8 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get -y -o Dpkg::Options::="--force-confnew" dist-upgrade
 apt-get -y install xfce4 xfce4-goodies lightdm eclipse
-) 2>&1 | logger -t firstboot -s 2>&1
+) 2>&1 | logger -i -t firstboot -s 2>&1
+
+logger -i -t firstboot "** $0 COMPLETE AFTER $SECOND SECOND(S)"
+
+exit 0
