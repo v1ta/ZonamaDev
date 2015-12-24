1decade9eeeb4cb795e666701b98b3558a707c7f
Karl Bunch <karlbunch@karlbunch.com>
2015-12-23 20:02:33 -0500

    Optimize loading

--- guest/firstboot.sh
+++ guest/firstboot.sh
@@ -7,13 +7,9 @@
 # Created: Wed Dec 23 19:14:02 EST 2015
 #
 
-logger -i -t firstboot "0=$0 BASH_SOURCE[0]=${BASH_SOURCE[0]}"
-
 pushd $(dirname ${BASH_SOURCE[0]}) > /dev/null
 export ME=$(pwd -P)'/'$(basename ${BASH_SOURCE[0]})
 popd > /dev/null
-
-logger -i -t firstboot "0=$0 BASH_SOURCE[0]=${BASH_SOURCE[0]} ME=$ME"
 
 (
     msg() {
@@ -22,31 +18,6 @@ logger -i -t firstboot "0=$0 BASH_SOURCE[0]=${BASH_SOURCE[0]} ME=$ME"
     }
 
     msg "START $ME (git: "$(git describe --always)" md5:"$(md5sum $ME)")"
-
-    unset UCF_FORCE_CONFFOLD
-    export UCF_FORCE_CONFFNEW=YES
-    ucf --purge /boot/grub/menu.lst
-
-    export DEBIAN_FRONTEND=noninteractive
-    apt-get update
-    apt-get -y -o Dpkg::Options::="--force-confnew" dist-upgrade
-    apt-get -y install dkms build-essential linux-headers-$(uname -r) xfce4 xfce4-goodies lightdm eclipse
-    apt-get -y autoremove
-
-    msg "Install Google Chrome"
-
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-    echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list
-    apt-get update
-    apt-get -y install google-chrome-stable
-
-    msg "Apply Patches"
-
-    for i in $(dirname ${BASH_SOURCE[0]})'/patches/'*
-    do
-	msg "Apply patch $i"
-	(cd /;patch -p0 -Nft) < "$i"
-    done
 
     msg "Unpack Tarballs"
 
@@ -60,8 +31,41 @@ logger -i -t firstboot "0=$0 BASH_SOURCE[0]=${BASH_SOURCE[0]} ME=$ME"
 
     usermod -c "vagrant" vagrant
 
+    msg "Update Packages"
+
+    # Add Googles's chrome repo to sources
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list
+
+    # Make sure we don't prompt with confusing things
+    unset UCF_FORCE_CONFFOLD
+    export UCF_FORCE_CONFFNEW=YES
+    ucf --purge /boot/grub/menu.lst
+
+    export DEBIAN_FRONTEND=noninteractive
+
+    # Get latest repo locations
+    apt-get update
+
+    # Upgrade whatever we can
+    apt-get -y -o Dpkg::Options::="--force-confnew" dist-upgrade
+
+    msg "Install Base Packages"
+
+    apt-get -y install dkms build-essential linux-headers-$(uname -r) xfce4 xfce4-goodies lightdm eclipse google-chrome-stable
+
+    apt-get -y autoremove
+
+    msg "Apply Patches"
+
+    for i in $(dirname ${BASH_SOURCE[0]})'/patches/'*
+    do
+	msg "Apply patch $i"
+	(cd /;patch -p0 -Nft) < "$i"
+    done
+
 ) 2>&1 | logger -i -t firstboot -s 2>&1
 
-logger -i -t firstboot "** $0 COMPLETE AFTER $SECONDS SECOND(S)"
+logger -i -t firstboot -s "** $0 COMPLETE AFTER $SECONDS SECOND(S)"
 
 exit 0
