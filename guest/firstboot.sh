d4da2917a8c0b94b078fe00314e920f8c92effd6
Karl Bunch <karlbunch@karlbunch.com>
2015-12-24 11:38:45 -0500

    Change firstboot to pass errors up to vagrant

--- guest/firstboot.sh
+++ guest/firstboot.sh
@@ -7,76 +7,98 @@
 # Created: Wed Dec 23 19:14:02 EST 2015
 #
 
-# Switch to an empty vt on console
-chvt 8
+export PACKAGES="dkms build-essential linux-headers-$(uname -r) xfce4 xfce4-goodies lightdm eclipse google-chrome-stable vim vim-doc vim-scripts avahi-daemon"
 
 pushd $(dirname ${BASH_SOURCE[0]}) > /dev/null
 export ME=$(pwd -P)'/'$(basename ${BASH_SOURCE[0]})
 popd > /dev/null
 
-export PACKAGES="dkms build-essential linux-headers-$(uname -r) xfce4 xfce4-goodies lightdm eclipse google-chrome-stable tree vim vim-doc vim-scripts"
+if [ "X$FISTBOOT_MAGIC" = "X" -a "X$1" = "X" ]; then
+    export FISTBOOT_MAGIC=magic
+    # Switch to an empty vt on console
+    chvt 8
+    apt-get -y install moreutils | tee /dev/console
+    exec $ME - 2>&1 | ts -s | logger -i -t firstboot -s 2>&1 | tee /dev/console
+    logger -i -t firstboot -s "** FAILED TO EXEC: $ME ** ABORT **"
+    exit 11
+fi
+
+## Should be magic from here on.. :-)
 export EXTRAS=$(egrep -hv '^#' extras ~vagrant/extras $(dirname $ME)/extras 2> /dev/null)
 
-apt-get -y install moreutils | tee /dev/console
+msg() {
+    local hd="##"$(echo "$1"|sed 's/./#/g')"##"
+    echo -e "$hd\n# $1 #\n$hd"
+}
 
-(
-    msg() {
-	local hd="+-"$(echo "$1"|sed 's/./-/g')"-+"
-	echo -e "$hd\n| $1 |\n$hd"
-    }
+msg "START $ME (git: "$(cd $(dirname $ME);git describe --always)" md5:"$(md5sum $ME)")"
 
-    msg "START $ME (git: "$(cd $(dirname $ME);git describe --always)" md5:"$(md5sum $ME)")"
+msg "Unpack Tarballs"
 
-    msg "Unpack Tarballs"
+for i in $(dirname $ME)'/tarballs/'*
+do
+    msg "unpack $i"
+    (umask 0;cd ~vagrant;tar xpvf $i)
+done
 
-    for i in $(dirname $ME)'/tarballs/'*
-    do
-	msg "unpack $i"
-	(umask 0;cd ~vagrant;tar xpvf $i)
-    done
+msg "Cusomize system"
 
-    msg "Cusomize system"
+usermod -c "vagrant" vagrant
 
-    usermod -c "vagrant" vagrant
+msg "Update Packages"
 
-    msg "Update Packages"
+# Add Googles's chrome repo to sources
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
 
-    # Add Googles's chrome repo to sources
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-    echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+# Make sure we don't prompt with confusing things
+unset UCF_FORCE_CONFFOLD
+export UCF_FORCE_CONFFNEW=YES
+ucf --purge /boot/grub/menu.lst
 
-    # Make sure we don't prompt with confusing things
-    unset UCF_FORCE_CONFFOLD
-    export UCF_FORCE_CONFFNEW=YES
-    ucf --purge /boot/grub/menu.lst
+export DEBIAN_FRONTEND=noninteractive
 
-    export DEBIAN_FRONTEND=noninteractive
+# Trap various failures
+trap 'msg "UNEXPECTED EXIT!"' 0
+trap 'msg "UNEXPECTED SIGNAL SIGHUP!";exit 21' HUP
+trap 'msg "UNEXPECTED SIGNAL SIGINT!";exit 22' INT
+trap 'msg "UNEXPECTED SIGNAL SIGTERM!";exit 23' TERM
+# exit if anything returns error
+set -e
 
-    # Get latest repo locations
-    apt-get update
+# Get latest repo locations
+apt-get update
 
-    # Upgrade whatever we can
-    apt-get -y -o Dpkg::Options::="--force-confnew" dist-upgrade
+# Upgrade whatever we can
+apt-get -y -o Dpkg::Options::="--force-confnew" dist-upgrade
 
-    msg "Install Packages"
+msg "Install Packages"
 
-    echo ">> PACKAGES: $PACKAGES $EXTRAS"
+echo ">> PACKAGES: $PACKAGES $EXTRAS"
 
-    apt-get -y install $PACKAGES $EXTRAS
+apt-get -y install $PACKAGES $EXTRAS
 
-    apt-get -y autoremove
+apt-get -y autoremove
 
-    msg "Apply Patches"
+systemctl set-default -f multi-user.target
 
-    for i in $(dirname ${BASH_SOURCE[0]})'/patches/'*
-    do
-	msg "Apply patch $i"
-	(cd /;patch -p0 -Nft) < "$i"
-    done
+msg "Apply Patches"
 
-    systemctl set-default -f multi-user.target
-) 2>&1 | ts -s | logger -i -t firstboot -s 2>&1 | tee /dev/console
+for i in $(dirname ${BASH_SOURCE[0]})'/patches/'*
+do
+    msg "Apply patch $i"
+    if (cd /;exec patch --verbose -p0 -Nft) < "$i"; then
+	msg "Patch $i SUCCESS"
+    else
+	msg "Patch $i failed! ret=$?"
+	exit 12
+    fi
+done
+
+chown -R vagrant:vagrant ~vagrant
 
 logger -i -t firstboot -s "** $0 COMPLETE AFTER $SECONDS SECOND(S)"
+
+trap - 0
 
 exit 0
