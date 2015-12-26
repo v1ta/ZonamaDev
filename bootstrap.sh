2d05654f71a39a77ba8e7f5c4d796e301ae99fb7
Karl Bunch <karlbunch@karlbunch.com>
2015-12-25 19:28:04 -0500

    Next rev of split build

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,64 @@
 #!/bin/bash
 
-exec < /dev/tty
+if [ -z "$BASH_VERSION" ]; then
+    echo "** MUST RUN FROM bash, please run again from bash! **"
+    exit
+fi
 
-## Magic here
+main() {
+    OS='unknown'
 
-## Check for git
+    case $(uname -s) in
+	Darwin ) OS='osx' ;;
+	*Linux* ) OS='linux' ;;
+	*_NT* ) OS='win';;
+	* ) echo "Not sure what OS you are on, guessing Windows"; OS='win';;
+    esac
 
-## Clone Repo
+    ## Check for git
 
-## hand off to next script
+    if git --version > /dev/null 2>&1; then
+	:
+    else
+	eval install_git_$OS()
+    fi
+
+    ## Clone Repo
+    git clone https://github.com/lordkator/ZonamaDev.git
+
+    ## hand off to next script
+    cd ZonamaDev/faststart
+    exec ./setup.sh < /dev/tty
+
+    echo "** Something went wrong, get help **"
+    exit 11
+}
+
+install_git_win() {
+    echo "Please download and install git-for-windows at: https://git-for-windows.github.io/"
+    echo "When that is complete, please use Git Bash shell to run this script again"
+    exit 0
+}
+
+install_git_osx() {
+    echo "Please download XCode for OSX at: https://developer.apple.com/xcode/downloads/"
+    open https://developer.apple.com/xcode/downloads/
+    echo "When that is complete, please restart this script."
+    exit 0
+}
+
+install_git_linux() {
+    # Assume deb for now?
+    sudo apt-get install git < /dev/tty
+
+    if git --version > /dev/null 2>&1; then
+	:
+    else
+	echo "** Failed to install git, **ABORT**"
+	exit 12
+    fi
+}
+
+main
 
 exit 0
---
+++ faststart/setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# setup.sh - Build the fast track box from the basebox in atlas
+#
+# Author: Lord Kator <lordkator@swgemu.com>
+#
+# Created: Fri Dec 25 18:50:00 EST 2015
+#
+
+if [ -z "$BASH_VERSION" ]; then
+    echo "** Please use BASH to run this script **"
+    exit 1
+fi
+
+main() {
+    if vagrant login --check; then
+	echo "** You're logged in, if you're using a pre-release copy you should make sure you're in the swgemu org **"
+    else
+	echo "** You're not logged in, if you're trying to use a pre-release box you're going to fail! **"
+
+	until vagrant login --check; do
+	    if yorn "** Do you want to login now? Y\b"; then
+		vagrant login
+	    else
+		break
+	    fi
+	done
+    fi
+
+    echo "vagrant up"
+    time vagrant up
+}
+
+yorn() {
+    echo -n -e "$@ Y\b"
+    read yorn
+    case $yorn in
+	[Nn]* ) return 1;;
+    esac
+    return 0
+}
+
+main
+
+exit 0
+
+# vi: ft=sh sw=4 cursorline cursorcolumn
