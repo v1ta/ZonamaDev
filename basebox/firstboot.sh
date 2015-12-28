2ce045924302984c64ff76c1133663971fbf4a55
Karl Bunch <karlbunch@karlbunch.com>
2015-12-28 07:05:18 -0500

    debug install problem

--- basebox/firstboot.sh
+++ basebox/firstboot.sh
@@ -112,7 +112,8 @@ ucf --purge /boot/grub/menu.lst
 export DEBIAN_FRONTEND=noninteractive
 
 # opts='-o Dpkg::Options::="--force-confnew" -o APT::Install-Suggests="false" -o Debug::pkgDPkgPM=true --no-install-recommends'
-opts='-o Dpkg::Options::="--force-confnew" -o APT::Install-Suggests="false" --no-install-recommends'
+# opts='-o Dpkg::Options::="--force-confnew" -o APT::Install-Suggests="false" --no-install-recommends'
+opts='-o Dpkg::Options::="--force-confnew" --no-install-recommends'
 
 set -x
 # exit if anything returns error
