ec2de5347f5b4e0ef7f96d629dc99ea8bab6b656
Karl Bunch <karlbunch@karlbunch.com>
2015-12-29 06:04:04 -0500

    Cleanup output, remove email too

--- basebox/scripts/package-prep.sh
+++ basebox/scripts/package-prep.sh
@@ -24,16 +24,19 @@ apt-get --yes clean
 rm /var/lib/dhcp/*
 
 # Zero sawp
+echo ">> Zero swap"
 readonly swapuuid=$(/sbin/blkid -o value -l -s UUID -t TYPE=swap)
 readonly swappart=$(readlink -f /dev/disk/by-uuid/"$swapuuid")
 /sbin/swapoff "$swappart"
-dd if=/dev/zero of="$swappart" bs=1M
+dd if=/dev/zero of="$swappart" bs=1M > /dev/null 2>&1
+echo ">> Make new swap"
 /sbin/mkswap -U "$swapuuid" "$swappart"
 
 chown -R vagrant:vagrant ~vagrant
 
 # Clean up any misc stuff in dev's user account
-(cd ~vagrant; rm -rf .suspend_devsetup .bash* .profile .inputrc .vim* .cache)
+echo ">> Cleanup user files that shouldn't be in the base box image."
+(cd ~vagrant; rm -rf .suspend_devsetup .bash* .profile .inputrc .vim* .cache /var/mail/*) 2> /dev/null
 
 # Stop all the noise
 service lightdm stop
@@ -49,23 +52,25 @@ vbpid=$(cat /var/run/vboxadd-service.pid 2> /dev/null)
 # Cleanup all the logs
 find /var/log /etc/machine-id /var/lib/dbus/machine-id -type f | while read fn
 do
-    echo "Zero $fn"
+    echo ">> Zero $fn"
     cp /dev/null "$fn"
 done
 
-echo "Fill filesystem with 0 bytes to reduce box size"
+echo ">> Fill filesystem with 0 bytes to reduce box size"
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
 
-echo "Sync disk"
+echo ">> Sync disk"
 sync; sync
 
-echo "** HALTING **"
+echo "***********************"
+echo "** HALT AND POWEROFF **"
+echo "***********************"
 
 set -x
 
 halt --force --no-wall --poweroff
 
-echo "** SHOULD NOT BE HERE!! **"
+echo "** SHOULD NOT BE HERE! GET HELP! **"
 
 exit 1
