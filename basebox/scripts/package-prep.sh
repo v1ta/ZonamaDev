e66f0a3f8f5d4bcc205f91be0b78bcc1c00ae1d1
Karl Bunch <karlbunch@karlbunch.com>
2015-12-30 06:13:43 -0500

    Remove .ssh/config

--- basebox/scripts/package-prep.sh
+++ basebox/scripts/package-prep.sh
@@ -106,7 +106,7 @@ chown -R vagrant:vagrant ~vagrant
 echo ">> Cleanup user files that shouldn't be in the base box image."
 (
     cd ~vagrant
-    rm -rf .suspend_devsetup .bash* .profile .inputrc .vim* .cache /var/mail/* .ssh/id_rsa* .visual .gerrit_username .mysql_history
+    rm -rf .suspend_devsetup .bash* .profile .inputrc .vim* .cache /var/mail/* .ssh/id_rsa* .ssh/config .visual .gerrit_username .mysql_history
     rm -rf .xsession* .gitconfig .lesshst .ssh/id_* .subversion workspace .cache
     sed -e '/ vagrant$/p' -e 'd' -i .ssh/authorized_keys
     mysql -e 'drop database swgemu' > /dev/null 2>&1 ;
