a4f77f6b8ddc1a931150a498fec88ed7a90ed0e6
Karl Bunch <karlbunch@karlbunch.com>
2015-12-31 17:50:51 -0500

    Check for needed commands, ask for upgrade if missing, closes #52

--- fasttrack/tre.sh
+++ fasttrack/tre.sh
@@ -141,11 +141,29 @@ msg() {
     echo -e "$hd\n# $1 #\n$hd"
 }
 
+check_cmds() {
+    for i in tty mktemp sed scp ssh find
+    do
+	if type -P $i > /dev/null; then
+	    :
+	else
+	    echo "** You're missin the $i command, you need to upgrade git for windows"
+	    echo "** Please download and install the latest from: https://git-for-windows.github.io/"
+	    exit
+	fi
+    done
+
+    return 1
+}
+
+
 declare -a trefiles
 trefiles=('bottom.tre' 'data_animation_00.tre' 'data_music_00.tre' 'data_other_00.tre' 'data_sample_00.tre' 'data_sample_01.tre' 'data_sample_02.tre' 'data_sample_03.tre' 'data_sample_04.tre' 'data_skeletal_mesh_00.tre' 'data_skeletal_mesh_01.tre' 'data_sku1_00.tre' 'data_sku1_01.tre' 'data_sku1_02.tre' 'data_sku1_03.tre' 'data_sku1_04.tre' 'data_sku1_05.tre' 'data_sku1_06.tre' 'data_sku1_07.tre' 'data_static_mesh_00.tre' 'data_static_mesh_01.tre' 'data_texture_00.tre' 'data_texture_01.tre' 'data_texture_02.tre' 'data_texture_03.tre' 'data_texture_04.tre' 'data_texture_05.tre' 'data_texture_06.tre' 'data_texture_07.tre' 'default_patch.tre' 'patch_00.tre' 'patch_01.tre' 'patch_02.tre' 'patch_03.tre' 'patch_04.tre' 'patch_05.tre' 'patch_06.tre' 'patch_07.tre' 'patch_08.tre' 'patch_09.tre' 'patch_10.tre' 'patch_11_00.tre' 'patch_11_01.tre' 'patch_11_02.tre' 'patch_11_03.tre' 'patch_12_00.tre' 'patch_13_00.tre' 'patch_14_00.tre' 'patch_sku1_12_00.tre' 'patch_sku1_13_00.tre' 'patch_sku1_14_00.tre')
 
 let "i=${#trefiles[@]} - 1"
 last_tre=${trefiles[$i]}
+
+check_cmds
 
 main
 
