ff0ebfca344dd0e89c151293435e589082d56a3c
Karl Bunch <karlbunch@karlbunch.com>
2016-02-08 06:35:05 -0500

    Add version checking and other tests, close #75

--- bootstrap.sh
+++ bootstrap.sh
@@ -49,11 +49,19 @@ main() {
 	eval install_git_$OS
     fi
 
+    if [ "$OS" = "win" ]; then
+	echo "** Checking for Git Bash **"
+	check_gitbash_$OS
+    fi
+
     echo "** Checking for VirtualBox **"
     check_virtualbox_$OS
 
     echo "** Checking for Vagrant **"
     check_vagrant_$OS
+
+    # If we're under the ZonamaDev dir back out to parent
+    cd ${PWD/ZonamaDev*/}
 
     ## Clone Repo
     if git clone https://github.com/lordkator/ZonamaDev.git; then
@@ -61,9 +69,13 @@ main() {
     else
 	case $PWD in
 	*ZonamaDev* ) : ;;
-	* ) echo "** Something is wrong, did you try and run this in the right directory? **"
-	    echo "** We suggest you run it from $HOME **"
-	    exit 1
+	* ) if [ -d ZonamaDev ]; then
+	        cd ZonamaDev
+	    else
+		echo "** Something is wrong, did you try and run this in the right directory? **"
+		echo "** We suggest you run it from $HOME **"
+		exit 1
+	    fi
 	    ;;
 	esac
 
@@ -113,7 +125,25 @@ install_git_linux() {
 }
 
 check_gitbash_win() {
-    echo "TODO check git bash version"
+    case $BASH_VERSION in
+	4.[3-9]* ) : ;;
+	* ) echo "Unsupported version of BASH (${BASH_VERSION}), please upgrade to BASH 4.3.x+"
+	    exit 1
+	    ;;
+    esac
+
+    for i in tty mktemp sed scp ssh find cygpath
+    do
+	if type -P $i > /dev/null; then
+	    :
+	else
+	    echo "** You're missing the $i command, you need to upgrade git for windows"
+	    echo "** Please download and install the latest from: https://git-for-windows.github.io/"
+	    exit 1
+	fi
+    done
+
+    return 1
 }
 
 check_virtualbox_win() {
@@ -126,6 +156,15 @@ check_virtualbox_win() {
 
 	echo "** Please close this window, install VirtualBox, REBOOT and try again **"
 	exit 1
+    else
+	local ver=$("${VBOX_MSI_INSTALL_PATH:-${VBOX_INSTALL_PATH}}/VBoxManage" --version)
+
+	case $ver in
+	    5.[0-9].1[2-9]* ) : ;;
+	    * ) echo "Unsupported version of virtualbox ($ver), please upgrade to 5.0.12 or higher"
+		exit 1
+		;;
+	esac
     fi
 }
 
@@ -142,6 +181,13 @@ check_vagrant_win() {
 	echo "** Please close this window, install Vagrant and try again **"
 	exit 1
     fi
+
+    case $ver in
+	1.[8-9].[1-9]* ) : ;;
+	* ) echo "Unsupported version of Vagrant ($ver), please upgrade to v1.8.1 or higher"
+	    exit 2
+	    ;;
+    esac
 
     echo "** Vagrant version $ver **"
 }
