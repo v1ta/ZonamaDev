9f9019fa07a3dae62f41e9034aab239659b47275
Karl Bunch <karlbunch@karlbunch.com>
2016-02-13 17:34:47 -0500

    First pass at #13, needs lots of testing

--- common/rundir.sh
+++ common/rundir.sh
@@ -20,9 +20,27 @@ if [ -z "$ME" ]; then
     fi
 fi
 
-if [ -z "$ZONAMADEV_CONFIG_HOME" ]; then
-    export ZONAMADEV_CONFIG_HOME=${XDG_CONFIG_HOME:-/home/vagrant/.config}/ZonamaDev
+# Hunt for global.config
+if [ -f "$HOME/ZonamaDev/common/global.config" ]; then
+    export ZDCFGPATH="${HOME}/common/global.config"
+else
+    dir=$(dirname $ME)
+    for i in "" "/.." "/../.." "/../../.."
+    do
+	cfg="${dir}${i}/common/global.config"
+
+	if [ -f "${cfg}" ]; then
+	    export ZDCFGPATH="${cfg}"
+	fi
+    done
 fi
+
+if [ -z "${ZDCFGPATH}" -o ! -f "${ZDCFGPATH}" ]; then
+    echo "** ERROR: Can not find global.config, GET HELP!"
+    exit 252
+fi
+
+source "${ZDCFGPATH}"
 
 TAG=$(basename $ME)
 RUN_STEP="init"
